86462b695bf4bf195ae0f85dd67ee3889d66cd15
vishal <vishal88kh@gmail.com>
2023-10-16 01:50:06 +0530

    added TracnscationComponent

---
+++ money-tracker/src/Components/TranscationList.tsx
@@ -0,0 +1,14 @@
+
+
+export const TranscationList = () => {
+  return (
+    <>
+        <h3>History</h3>
+        <ul className="list">
+            <li className="minus">
+                Cash <span>-$400</span> <button className="delete-btn">X</button>
+            </li>
+        </ul>
+    </>
+  )
+}
