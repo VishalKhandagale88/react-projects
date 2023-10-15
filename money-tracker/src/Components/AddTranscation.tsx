97256b2eaf77fc5f60736ec38c3ac8cc9809157f
vishal <vishal88kh@gmail.com>
2023-10-16 02:10:12 +0530

    added AddTransaction component

---
+++ money-tracker/src/Components/AddTranscation.tsx
@@ -0,0 +1,20 @@
+export const AddTranscation = () => {
+  return (
+    <>
+        <h3>Add new trasaction</h3>
+        <form >
+            <div className="form-control">
+                <label htmlFor="text">Test</label>
+                <input type="text" placeholder="Enter test ..." />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">Amount<br>
+                </br>(negative - expression, positive -income)
+                </label>
+                <input type="number" placeholder="Enter amount ...." />
+            </div>
+             <button className="btn">Add transcation</button>
+        </form>
+    </>
+  )
+}
