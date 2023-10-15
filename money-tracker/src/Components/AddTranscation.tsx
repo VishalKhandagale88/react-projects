3456ec8e41258f6c2bcf0ea7aa0bff544eb92f89
vishal <vishal88kh@gmail.com>
2023-10-16 02:50:47 +0530

    added logic to store data in a variable

--- money-tracker/src/Components/AddTranscation.tsx
+++ money-tracker/src/Components/AddTranscation.tsx
@@ -1,18 +1,21 @@
 import "../css/AddTranscationCss.css"
+import { useState } from "react";
 export const AddTranscation = () => {
+    const [text, setText] = useState("");
+    const [amount,setAmount] = useState(0);
   return (
     <>
         <h3>Add new trasaction</h3>
         <form >
             <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text" placeholder="Enter test ..." />
+                <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter test ..." />
             </div>
             <div className="form-control">
                 <label htmlFor="amount">Amount<br>
                 </br>(negative - expression, positive -income)
                 </label>
-                <input type="number" placeholder="Enter amount ...." />
+                <input type="number" value={amount} onChange={ (e)=>setAmount(parseFloat(e.target.value)) } placeholder="Enter amount ...." />
             </div>
              <button className="btn">Add transcation</button>
         </form>
