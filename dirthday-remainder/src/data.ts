4c736b82a6dd446e02f2f4cb64fa755d3a91c2a9
vishal <vishal.khandagale@wondrx.info>
2024-02-25 20:06:27 +0530

    added data file

---
+++ dirthday-remainder/src/data.ts
@@ -0,0 +1,33 @@
+export default[
+    {
+        id:1,
+        name:"John Doe",
+        age:20,
+        image:"https://unsplash.com/photos/focus-photography-of-mans-face-8CEKQlmnf50"
+    },{
+        id:2,
+        name:"Alise Nath",
+        age:22,
+        image:"https://unsplash.com/photos/woman-with-green-blue-and-purple-face-paint-M8YFCSn7YuA"
+    },{
+        id:3,
+        name:"Jhon Cena",
+        age:28,
+        image:"https://unsplash.com/photos/smiling-woman-holding-cheek-B4TjXnI0Y2c"
+    },{
+        id:4,
+        name:"Doe cnae",
+        age:25,
+        image:"https://unsplash.com/photos/woman-with-green-blue-and-purple-face-paint-M8YFCSn7YuA"
+    },{
+        id:5,
+        name:"Yashwant Mhamane",
+        age:26,
+        image:"https://unsplash.com/photos/man-in-black-crew-neck-shirt-DYQDKXrAkyg"
+    },{
+        id:6,
+        name:"Yernsh Nomad",
+        age:23,
+        image:"https://unsplash.com/photos/men-looking-far-during-daytime-close-up-photography-TeAj27VRKzg"
+    }
+]
